ad435c0026722b33f4fc175c5aa2b579719b54ad
sonson469 <sonson.owata@icloud.com>
2021-10-12 17:11:21 +0900

    Excelから名前参照して同名プレハブ生成的な

--- MS_Game/Assets/Lee/Script/CButtonClick.cs
+++ MS_Game/Assets/Lee/Script/CButtonClick.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CButtonClick : MonoBehaviour
 {
-    [SerializeField] private TreeDatas m_TreeData;
+    [SerializeField] private TreeData m_TreeData;
     private CPrefabList m_PrefabList;
+
     // Start is called before the first frame update
     void Start()
     {
         m_PrefabList = this.gameObject.GetComponent<CPrefabList>();
+
     }
 
     // Update is called once per frame
@@ -20,9 +22,13 @@ public class CButtonClick : MonoBehaviour
 
     public void CeateTree(int TreeID)
     {
-        if(m_TreeData.sheets[0].list[TreeID-1].Cost <= 400)
+        if(m_TreeData.sheets[0].list[TreeID-1].Cost <= 10000)
         {
-            m_PrefabList.TreeCreate(TreeID);
+
+            GameObject obj = (GameObject)Resources.Load(m_TreeData.sheets[0].list[TreeID - 1].Name);
+
+            // プレハブを元にオブジェクトを生成する
+            Instantiate(obj,new Vector3(5.0f, 0.0f, 0.0f),Quaternion.identity);
         }
     }
 }
