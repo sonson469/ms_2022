3a1a3f79781b746920a0f12001a2c8bfa479a1b6
sonson469 <sonson.owata@icloud.com>
2021-10-12 23:34:19 +0900

    移動

--- MS_Game/Assets/Lee/Script/CButtonClick.cs
+++ MS_Game/Assets/Lee/Script/CButtonClick.cs
@@ -6,12 +6,19 @@ public class CButtonClick : MonoBehaviour
 {
     [SerializeField] private TreeData m_TreeData;
     private CPrefabList m_PrefabList;
+    private CObjectMove m_ObjectMoveScript;
+
+    private GameObject m_Object;
+
+    private GameObject m_TargetObject;
+
+    private Vector3 m_ObjectPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         m_PrefabList = this.gameObject.GetComponent<CPrefabList>();
-
+        m_ObjectMoveScript = this.gameObject.GetComponent<CObjectMove>();
     }
 
     // Update is called once per frame
@@ -25,10 +32,23 @@ public class CButtonClick : MonoBehaviour
         if(m_TreeData.sheets[0].list[TreeID-1].Cost <= 10000)
         {
 
-            GameObject obj = (GameObject)Resources.Load(m_TreeData.sheets[0].list[TreeID - 1].Name);
+            m_Object = (GameObject)Resources.Load(m_TreeData.sheets[0].list[TreeID - 1].Name);
+
+            m_ObjectPosition = new Vector3(5.0f, 0.0f, 0.0f);
 
             // プレハブを元にオブジェクトを生成する
-            Instantiate(obj,new Vector3(5.0f, 0.0f, 0.0f),Quaternion.identity);
+            m_TargetObject = Instantiate(m_Object,m_ObjectPosition,Quaternion.identity);
+
+            m_ObjectMoveScript.SetTargetObject(m_TargetObject);
+
+            m_ObjectMoveScript.SetObjectMove(true);
+
+            m_ObjectMoveScript.SetPlane();
         }
+    }
+
+    public Vector3 GetObjectPosition()
+    {
+        return m_ObjectPosition;
     }
 }
