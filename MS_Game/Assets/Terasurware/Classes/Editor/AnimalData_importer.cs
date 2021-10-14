43b9bfa1244a1542c34f95d7ec6172cce9537a2e
sonson469 <sonson.owata@icloud.com>
2021-10-14 14:54:57 +0900

    樹木、低樹木のPrefab用意、自分のシーンにUI移した、動物と動物の巣エクセル読み込んだとこまで

---
+++ MS_Game/Assets/Terasurware/Classes/Editor/AnimalData_importer.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using System.Xml.Serialization;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+public class AnimalData_importer : AssetPostprocessor {
+	private static readonly string filePath = "Assets/Lee/Excel/AnimalData.xls";
+	private static readonly string exportPath = "Assets/Lee/Excel/AnimalData.asset";
+	private static readonly string[] sheetNames = { "Sheet1", };
+	
+	static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		foreach (string asset in importedAssets) {
+			if (!filePath.Equals (asset))
+				continue;
+				
+			AnimalData data = (AnimalData)AssetDatabase.LoadAssetAtPath (exportPath, typeof(AnimalData));
+			if (data == null) {
+				data = ScriptableObject.CreateInstance<AnimalData> ();
+				AssetDatabase.CreateAsset ((ScriptableObject)data, exportPath);
+				data.hideFlags = HideFlags.NotEditable;
+			}
+			
+			data.sheets.Clear ();
+			using (FileStream stream = File.Open (filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
+				IWorkbook book = null;
+				if (Path.GetExtension (filePath) == ".xls") {
+					book = new HSSFWorkbook(stream);
+				} else {
+					book = new XSSFWorkbook(stream);
+				}
+				
+				foreach(string sheetName in sheetNames) {
+					ISheet sheet = book.GetSheet(sheetName);
+					if( sheet == null ) {
+						Debug.LogError("[QuestData] sheet not found:" + sheetName);
+						continue;
+					}
+
+					AnimalData.Sheet s = new AnimalData.Sheet ();
+					s.name = sheetName;
+				
+					for (int i=1; i<= sheet.LastRowNum; i++) {
+						IRow row = sheet.GetRow (i);
+						ICell cell = null;
+						
+						AnimalData.Param p = new AnimalData.Param ();
+						
+					cell = row.GetCell(0); p.Id = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(1); p.Name = (cell == null ? "" : cell.StringCellValue);
+						s.list.Add (p);
+					}
+					data.sheets.Add(s);
+				}
+			}
+
+			ScriptableObject obj = AssetDatabase.LoadAssetAtPath (exportPath, typeof(ScriptableObject)) as ScriptableObject;
+			EditorUtility.SetDirty (obj);
+		}
+	}
+}
---
+++ MS_Game/Assets/Terasurware/Classes/Editor/AnimalNestData_importer.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using System.Xml.Serialization;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+public class AnimalNestData_importer : AssetPostprocessor {
+	private static readonly string filePath = "Assets/Lee/Excel/AnimalNestData.xls";
+	private static readonly string exportPath = "Assets/Lee/Excel/AnimalNestData.asset";
+	private static readonly string[] sheetNames = { "Sheet1", };
+	
+	static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		foreach (string asset in importedAssets) {
+			if (!filePath.Equals (asset))
+				continue;
+				
+			AnimalNestData data = (AnimalNestData)AssetDatabase.LoadAssetAtPath (exportPath, typeof(AnimalNestData));
+			if (data == null) {
+				data = ScriptableObject.CreateInstance<AnimalNestData> ();
+				AssetDatabase.CreateAsset ((ScriptableObject)data, exportPath);
+				data.hideFlags = HideFlags.NotEditable;
+			}
+			
+			data.sheets.Clear ();
+			using (FileStream stream = File.Open (filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
+				IWorkbook book = null;
+				if (Path.GetExtension (filePath) == ".xls") {
+					book = new HSSFWorkbook(stream);
+				} else {
+					book = new XSSFWorkbook(stream);
+				}
+				
+				foreach(string sheetName in sheetNames) {
+					ISheet sheet = book.GetSheet(sheetName);
+					if( sheet == null ) {
+						Debug.LogError("[QuestData] sheet not found:" + sheetName);
+						continue;
+					}
+
+					AnimalNestData.Sheet s = new AnimalNestData.Sheet ();
+					s.name = sheetName;
+				
+					for (int i=1; i<= sheet.LastRowNum; i++) {
+						IRow row = sheet.GetRow (i);
+						ICell cell = null;
+						
+						AnimalNestData.Param p = new AnimalNestData.Param ();
+						
+					cell = row.GetCell(0); p.Id = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(1); p.Name = (cell == null ? "" : cell.StringCellValue);
+					cell = row.GetCell(2); p.Cost = (int)(cell == null ? 0 : cell.NumericCellValue);
+						s.list.Add (p);
+					}
+					data.sheets.Add(s);
+				}
+			}
+
+			ScriptableObject obj = AssetDatabase.LoadAssetAtPath (exportPath, typeof(ScriptableObject)) as ScriptableObject;
+			EditorUtility.SetDirty (obj);
+		}
+	}
+}
