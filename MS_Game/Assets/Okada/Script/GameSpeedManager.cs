5ce7ae18f8deef88f1c59824a736a7fa2a9b62f2
OkadaShogo <senkou.8140989@gmail.com>
2021-10-21 12:55:11 +0900

    日数計算修正・資金の設定

--- MS_Game/Assets/Okada/Script/GameSpeedManager.cs
+++ MS_Game/Assets/Okada/Script/GameSpeedManager.cs
@@ -15,6 +15,9 @@ public class GameSpeedManager : MonoBehaviour
     private bool m_GameStop;
     private DateTime m_Date;
     private Calendar m_Calendar;
+
+    [SerializeField] private int m_Money;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,10 +32,10 @@ public class GameSpeedManager : MonoBehaviour
     {
         m_GameSpeedText.text = m_GameSpeedCount.ToString() + "Å~";
         //m_ElapsedTimeText.text = m_Date.Year.ToString()+"îN"+m_Date.Month.ToString()+"åé";
-        int weekNumber = m_Calendar.GetWeekOfYear(m_Date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday) - m_Calendar.GetWeekOfYear(new DateTime(m_Date.Year, m_Date.Month, 1), CalendarWeekRule.FirstDay, DayOfWeek.Sunday) + 1;
+        //int weekNumber = m_Calendar.GetWeekOfYear(m_Date, CalendarWeekRule.FirstDay, DayOfWeek.Sunday) - m_Calendar.GetWeekOfYear(new DateTime(m_Date.Year, m_Date.Month, 1), CalendarWeekRule.FirstDay, DayOfWeek.Sunday) + 1;
         //int isoWeekNumber = m_Calendar.GetWeekOfYear(m_Date, CalendarWeekRule.FirstDay, DayOfWeek.Monday) - m_Calendar.GetWeekOfYear(new DateTime(m_Date.Year, m_Date.Month, 1), CalendarWeekRule.FirstDay, DayOfWeek.Monday) + 1;
-
-        m_ElapsedTimeText.text = m_Date.Year.ToString() + "îN" + m_Date.Month.ToString() + "åé" + weekNumber.ToString() + "èT";
+        //m_ElapsedTimeText.text = m_Date.Year.ToString() + "îN" + m_Date.Month.ToString() + "åé" + weekNumber.ToString() + "èT";
+        m_ElapsedTimeText.text = m_Date.Day.ToString() + "ì˙";
     }
 
     public void ChangeGameSpeed()
@@ -51,23 +54,24 @@ public class GameSpeedManager : MonoBehaviour
         m_GameStop = !m_GameStop;
         if (m_GameStop)
         {
-            Time.timeScale = 0;
-        }
-        else
-        {
+
+        }else{
             Time.timeScale = m_GameSpeedCount;
         }
     }
 
     IEnumerator AddDay()
     {
-
         while (true)
         {
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(60.0f);
             m_Date = m_Date.AddDays(1);
             Debug.Log(m_Date.ToString());
         }
+    }
 
+    public int GetMoney()
+    {
+        return m_Money;
     }
 }
