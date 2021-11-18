cee3ce44e6e7e23a193e42c260af6b4d9844e943
sonson469 <sonson.owata@icloud.com>
2021-11-18 10:02:15 +0900

    動物Prefab変更とか

--- MS_Game/Assets/Okada/Script/AnimalMove.cs
+++ MS_Game/Assets/Okada/Script/AnimalMove.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class AnimalMove : MonoBehaviour
 {
     private Vector3 m_TargetPos;
-    [SerializeField] private float m_Min, m_Max;
+    private float m_Max;
     private NavMeshAgent m_NavMeshAgent;
     private NavMeshSurface m_NavMeshSurface;
     private GameObject m_TargetNest;
@@ -20,8 +20,11 @@ public class AnimalMove : MonoBehaviour
 
     void Update()
     {
-        if (!m_NavMeshAgent.pathPending && m_NavMeshAgent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        if (m_NavMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            if (!m_NavMeshAgent.pathPending && m_NavMeshAgent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
     }
 
     void GotoNextPoint()
@@ -30,7 +33,9 @@ public class AnimalMove : MonoBehaviour
         m_TargetPos.z = Random.Range(-m_Max, m_Max);
         //m_NavMeshSurface.BuildNavMesh();
         m_TargetPos.y = m_TargetNest.transform.position.y;
-        m_NavMeshAgent.SetDestination(m_TargetNest.transform.position + m_TargetPos);
+        Vector3 pos = m_TargetNest.transform.position + m_TargetPos;
+        m_NavMeshAgent.Warp(pos);
+        m_NavMeshAgent.SetDestination(pos);
     }
 
 
