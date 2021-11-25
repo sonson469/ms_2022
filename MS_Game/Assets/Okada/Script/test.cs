ee7df8a3ca7d2a7010b8133b0762d2e922f00f30
OkadaShogo <senkou.8140989@gmail.com>
2021-11-25 10:34:38 +0900

    セーブテスト

--- MS_Game/Assets/Okada/Script/test.cs
+++ MS_Game/Assets/Okada/Script/test.cs
@@ -5,15 +5,45 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Vector3 pos;
+
+    public CSaveObj SaveObj = new CSaveObj();
+
+    [System.Serializable]
+    public class CSaveObj
+    {
+        public CGameObject m_GameObj;
+        public CGameTimeManager m_GameTime;
+    }
 
     void Start()
     {
-        pos = transform.position;
+        CSaveManager.DeleteAll();
+       
     }
 
     void Update()
     {
-        transform.position = new Vector3(pos.x + Mathf.PingPong(Time.time, 5), pos.y, pos.z);
+        if (Input.GetKeyDown(KeyCode.J))
+            Save();
     }
+
+    private void Save()
+    {
+        //CSaveManager.SetInt("ii", SaveObj.m_GameObj.m_TreeNameCount[0]);
+        //CSaveManager.SetClass("GameObj", SaveObj.m_GameObj);
+        //CSaveManager.SetClass("GameTime", SaveObj.m_GameTime);
+        //CSaveManager.SetFloat("Day",SaveObj.m_GameTime.GetGameDay());
+        //CSaveManager.Save();
+
+        //CSaveObj obj = CSaveManager.GetClass<CSaveObj>("GameObj", SaveObj);
+        //Debug.Log(CSaveManager.GetInt("ii"));
+
+        //Debug.Log(SaveObj.m_GameObj.ToString());
+        //Debug.Log(obj.ToString());
+
+        CSaveManager.SetClass<CGameObject>("gameObj", SaveObj.m_GameObj);
+        Debug.Log("ÉZÅ[Éu");
+
+    }
+
 }
