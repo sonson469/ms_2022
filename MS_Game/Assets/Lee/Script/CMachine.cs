c308ea811b47b83fd5e90512e974d57931ecf879
sonson469 <sonson.owata@icloud.com>
2021-12-16 11:04:36 +0900

    機械の位置

--- MS_Game/Assets/Lee/Script/CMachine.cs
+++ MS_Game/Assets/Lee/Script/CMachine.cs
@@ -12,18 +12,33 @@ public class CMachine : MonoBehaviour
 
     private int m_Day;  //íuÇ¢ÇƒÇ©ÇÁÇÃåoâﬂì˙êî
 
-    private bool m_CanPut = true;
+    private bool m_CanPut = false;
 
     private bool m_Put = false;
 
     private bool m_Trigger = false;
 
+    private Vector3[] m_Position = new Vector3[9];
+    private bool[] m_CanPutPosition = new bool[9];
+
     void Start()
     {
+
         m_Manager = GameObject.FindWithTag("Manager");
         m_TimeManager = m_Manager.GetComponent<CGameTimeManager>();
 
         m_MyGround.SetActive(false);
+
+        m_Position[0] = new Vector3(-30.0f, 0.0f, 30.0f);
+        m_Position[1] = new Vector3(-30.0f, 0.0f, 0.0f);
+        m_Position[2] = new Vector3(-30.0f, 0.0f, -30.0f);
+        m_Position[3] = new Vector3(0.0f, 0.0f, -30.0f);
+        m_Position[4] = new Vector3(0.0f, 0.0f, 0.0f);
+        m_Position[5] = new Vector3(0.0f, 0.0f, 30.0f);
+        m_Position[6] = new Vector3(30.0f, 0.0f, 30.0f);
+        m_Position[7] = new Vector3(30.0f, 0.0f, 0.0f);
+        m_Position[8] = new Vector3(30.0f, 0.0f, -30.0f);
+
     }
 
     // Update is called once per frame
@@ -46,27 +61,37 @@ public class CMachine : MonoBehaviour
             }
         }
 
-        if(m_Trigger)
+        /*if(m_Trigger)
         {
             m_CanPut = false;
         }
         else if(!m_Trigger)
         {
             m_CanPut = true;
+        }*/
+
+        if (transform.position == m_Position[0] || transform.position == m_Position[1] || transform.position == m_Position[2] || transform.position == m_Position[3] || transform.position == m_Position[4] || transform.position == m_Position[5] || transform.position == m_Position[6] || transform.position == m_Position[7] || transform.position == m_Position[8])
+        {
+            m_CanPut = true;
         }
+        else
+        {
+            m_CanPut = false;
+        }
+
     }
 
     private void FixedUpdate()
     {
-        m_Trigger = false;
+        //m_Trigger = false;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Machine")
+        /*if (other.gameObject.tag == "Machine")
         {
             m_Trigger = true;
-        }
+        }*/
     }
 
     public bool GetCanPut()
